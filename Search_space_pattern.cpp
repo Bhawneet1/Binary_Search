8ef45ed5e629769f44ccc707450c176a6bf5f979
Bhawneet Singh <136002853+Bhawneet1@users.noreply.github.com>
2025-02-19 07:30:29 +0530

    Add files via upload

---
+++ Search_space_pattern.cpp
@@ -0,0 +1,9 @@
+// Search space pattern kehta hai ki answer kai space kai upar binary search lago gai
+
+//template
+//while()
+//{
+    // if(predicate()) return ans
+    // else if left
+    // else right
+//}
