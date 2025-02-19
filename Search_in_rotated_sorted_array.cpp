1af78aaf6580526d2057fc6f155e0dd35b189f54
Bhawneet Singh <136002853+Bhawneet1@users.noreply.github.com>
2025-02-19 07:21:40 +0530

    Add files via upload

--- Search_in_rotated_sorted_array.cpp
+++ Search_in_rotated_sorted_array.cpp
@@ -1,49 +1,74 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int find_pivot(vector<int> nums)
-{
-  int s=0;
-  int e=nums.size()-1;
-  while(s<=e)
-  {
-    int mid=s+(e-s)/2;
-    if(nums[mid]>nums[mid+1]) return mid;
-    if(nums[mid]<nums[mid-1]) return mid-1;
-    if(nums[s]>nums[mid]) e=mid-1;//means baad waala sequence hai
-    else s=mid+1;
-  }
-}
-int binary_search(vector<int> nums,int s,int e,int target)
-{
-  while(s<=e)
-  {
-    int mid=s+(e-s)/2;
-    if(nums[mid]==target)return mid;
-    else if(nums[mid]>target) e=mid-1;
-    else s=mid+1;
 
-  }
-  return -1;
+int getPivotIndex(vector<int> &arr)
+{
+    int s = 0;
+    int n = arr.size();
+    int e = n - 1;
+    int mid;
+
+    while (s < e)
+    {
+        mid = s + (e - s) / 2;
+        if (arr[mid] > arr[e])
+        {
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid;
+        }
+    }
+    return s;
 }
+
+int binarySearch(int s, int e, int target, vector<int> &arr)
+{
+    int mid;
+    while (s <= e)
+    {
+        mid = s + (e - s) / 2;
+        if (arr[mid] == target)
+        {
+            return mid;
+        }
+        if (target > arr[mid])
+        {
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int search(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    if (n == 0)
+    {
+        return -1;
+    }
+
+    int pivotIndex = getPivotIndex(nums);
+
+    if (nums[pivotIndex] <= target && target <= nums[n - 1])
+    {
+        return binarySearch(pivotIndex, n - 1, target, nums);
+    }
+    else
+    {
+        return binarySearch(0, pivotIndex - 1, target, nums);
+    }
+}
+
 int main()
 {
-  vector<int> nums{50,60,70,10,20,30,40};
-  int ans = find_pivot(nums);
-  int target=20;
-  // line A -> index 0 to pivotindex
-  // line B -> index pivotindex to n-1
-  // if target in line A then serach in line A
-  int n=nums.size();
-  if(target >nums[0] && target <nums[ans])
-  {
-    int ans=binary_search(nums,0,ans,target);
-    cout<<ans;
-  }
-  //target in line B
-  
-  else if(target>nums[ans+1] && target < nums[n-1]){
-      int ans=binary_search(nums,ans+1,n-1,target);
-      cout<<ans;
-  }
+    vector<int> nums{10, 30, 70, 10, 20, 30};
+    int target = 20;
+    int ans = search(nums, target);
+    cout << ans << endl;
 }
